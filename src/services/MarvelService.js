7b5ae4fa7ea986bfe18bb878a9b559cbb6d7377c
Alexander Tretyakov <verfenders@gmail.com>
2022-12-12 06:34:20 +0300

    new pannel

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,7 +1,7 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
-    _apiKey = 'apikey=5221cef91d070bdeae0cf1be56e7844f';
+    _apiKey = 'apikey=c5d6fc8b83116d92ed468ce36bac6c62';
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -23,20 +23,17 @@ class MarvelService {
         return this._transformCharacter(res.data.results[0]);
     }
 
-    
-
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
             description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
-
 }
 
 export default MarvelService;
-
-// https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=5221cef91d070bdeae0cf1be56e7844f
